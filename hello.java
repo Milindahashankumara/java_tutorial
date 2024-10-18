78c8954c4fb7388ad1eb4990d96654605f7e5841
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2024-10-18 22:50:07 +0530

    cal

--- hello.java
+++ hello.java
@@ -1,6 +1,23 @@
- class kkk{
+ import java.util.Scanner ;
+
+ public class hello{
+
     public static void main(String[] args){
 
-        System.out.print("Hello");
+        Scanner sun = new Scanner(System.in);
+        double num1,num2,ans;
+
+        System.out.println("Enter first Num: ");
+        num1 = sun.nextDouble();
+
+        System.out.println("Enter second Num: ");
+        num2 = sun.nextDouble();
+
+        ans = num1 + num2;
+
+
+        System.out.print("Answer is: " + ans);
+       
+
     }
-}
+ }
